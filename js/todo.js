a5a0bd45793f45fc91a8e5014bc17ba90d631dc7
Jay Angelo Valmocina <valmocinajayangelo@gmail.com>
2018-07-16 12:12:49 +0800

    added timer

--- js/todo.js
+++ js/todo.js
@@ -24,11 +24,28 @@ function addTodo() {
   for (var i in close) {
     close[i].addEventListener('click', function() { this.parentElement.style.display = "none"})
    }
-  
-
-
-
-  
-
-
+ 
 }
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 500);
+}
+
+// window.onload = function () {
+//     var fiveMinutes = 60 * 10,
+//         display = document.querySelector('#time');
+//     startTimer(fiveMinutes, display);
+// };
