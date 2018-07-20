fce7503bf1ed5e7e9c32ea638e3c1c91593098c2
Jay Angelo Valmocina <valmocinajayangelo@gmail.com>
2018-07-20 12:03:32 +0800

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -27,25 +27,4 @@ function addTodo() {
  
 }
 
-function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10)
-        seconds = parseInt(timer % 60, 10);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = minutes + ":" + seconds;
-
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 500);
-}
-
-window.onload = function () {
-    var fiveMinutes = 60 * 10,
-        display = document.querySelector('#time');
-    startTimer(fiveMinutes, display);
-};
